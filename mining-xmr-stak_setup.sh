59ece0451fb2aa384c92f67ba4a2505e0cff101a
minerdude <31804298+minerdude@users.noreply.github.com>
2017-10-21 15:03:57 -0300

    Add files via upload

--- mining-xmr-stak_setup.sh
+++ mining-xmr-stak_setup.sh
@@ -1,54 +1,38 @@
 #!/bin/bash
-
-#ConfiguraÁ„o inicial
-function inicialconf ()
-{
-local resposta="s"
-echo -e "Instalacao de pacotes necessarios...\n"
-sleep 1
-sudo apt update && sudo apt install unattended-upgrades nano git htop screen build-essential cmake libssl-dev libhwloc-dev -y
-sudo sed -i 's/#startup_message.*/startup_message off/' /etc/screenrc
-sudo sed -i 's/.*\${distro_id}:\${distro_codename}-updates.*/\t"\${distro_id}:\${distro_codename}-updates";/' /etc/apt/apt.conf.d/50unattended-upgrades
-# read USERNAME -p "Informe nome do usu·rio a criar: "
-# read PASSWORD -p "Informe a senha: "
-# adduser --quiet --disabled-password --gecos "User" "$USERNAME" && \
-# echo "$password" | sudo chpasswd <<< "$USERNAME":"$PASSWORD" && \ 
-# usermod -aG sudo "$username"
-# sudo sed -i 's/%sudo.*/%sudo ALL=(ALL:ALL) NOPASSWD:ALL/' /etc/sudoers
-# sudo sed -i 's/*.Port 22/Port 4443/' /etc/ssh/sshd_config
-# sudo sed -i 's/PermitRootLogin yes/PermitRootLogin no/' /etc/ssh/sshd_config
-echo -e "alias update='sudo apt update'\nalias upgrade='sudo apt upgrade'\nalias clean='sudo apt clean && sudo apt autoclean && sudo apt autoremove'\nalias xmr='./xmr-stak config.txt'\nalias upgradable='apt list --upgradable'" | tee -a ~/.bash_aliases
-source .bashrc
-# sudo service ssh restart
-return $?
-}
+tput setaf 7 ; tput setab 6 ; tput bold ; printf '%35s%s%-20s\n' "Configuracao Inicial do VPS Mining" ; tput sgr0
+tput setaf 3 ; tput bold ; echo "" ; echo "Este script ira compilar o xmr-stak-cpu, fazer configuracoes no sysctl.conf e" ; tput sgr0
+tput setaf 3 ; tput bold ; echo "/etc/security/limits.conf e instalar alguns pacotes uteis." ; echo "" ; tput sgr0
+tput setaf 3 ; tput bold ; echo -e "A instala√ß√£o iniciar√° em 3 segundos\n\n" ; tput sgr0 ; sleep 3
 
 #XMR-Stak-CPU compiling
-function stakmake ()
-{
-git clone -b dev git://github.com/fireice-uk/xmr-stak-cpu.git && \
-cd xmr-stak-cpu
+cd ~
+sudo apt update && sudo apt install build-essential cmake libssl-dev libhwloc-dev nano git htop screen -y
+sudo sed -i 's/#startup_message.*/startup_message off/' /etc/screenrc
+sudo sed -i 's/.*\${distro_id}:\${distro_codename}-updates.*/\t"\${distro_id}:\${distro_codename}-updates";/' /etc/apt/apt.conf.d/50unattended-upgrades
+git clone -b dev https://github.com/fireice-uk/xmr-stak-cpu && cd xmr-stak-cpu && \
 sed -i 's/constexpr double fDevDonationLevel.*/constexpr double fDevDonationLevel = 0.0;/' donate-level.h
 cmake .  -DMICROHTTPD_ENABLE=OFF  && \
-make -j $(nproc)
-if [ "$?" -eq "0" ]; then
-	cp bin/xmr-stak-cpu ~/xmr-stak
-	cp config.txt ~/config.txt
-	# echo "Configuracao do config.txt do Stak..."
-	# read -p "Informe quantos cores deseja utilizar: " ncores
-	# sed '/"low_power_mode"/d' ~/config.txt > /tmp/config && mv /tmp/config ~/config.txt
-	return $?
+make -j $(nproc) install
+if [ $? != 0 ]; then
+	echo -e "\nErro ao compilar! \nError exit code: $?" >&2
+	exit 1
 else
-	return 1
+	echo -e "\nXMR-Stak-CPU Compilado!\n"
 fi
-}
+cp bin/xmr-stak-cpu ~/xmr-stak
+sleep 1
+
+echo -e "Agora as configuracoes finais.\n"
+sleep 1
+
+#Aliases
+cd ~
+echo -e "alias update='sudo apt update'\nalias upgrade='sudo apt upgrade'\nalias clean='sudo apt clean && sudo apt autoclean && sudo apt autoremove'\nalias upgradable='apt list --upgradable'\nalias xmrstart='pm2 start stak.json'\nalias xmrstop='pm2 stop stak'" | tee -a ~/.bash_aliases
+source .bashrc
 
 #Sysctl Conf
-function confsysctl ()
-{
 if [  -f "/etc/sysctl.d/99-xmrmining.conf" ]; then
-	echo -e "\nSysconf j· configurado anteriormente.\n"
-	return 11
+	echo -e "\nSysconf j√° configurado anteriormente."
 else
 	echo -e "\n#Protect Against TCP Time-Wait\nnet.ipv4.tcp_rfc1337 = 1\n" | sudo tee /etc/sysctl.d/99-xmrmining.conf
 	echo -e "#Latency\nnet.ipv4.tcp_low_latency = 1\nnet.ipv4.tcp_slow_start_after_idle = 0\n" | sudo tee -a /etc/sysctl.d/99-xmrmining.conf
@@ -56,64 +40,17 @@ else
 	echo -e "#Do less swapping\nvm.swappiness = 10\nvm.dirty_ratio = 10\nvm.dirty_background_ratio = 5\nvm.vfs_cache_pressure = 50\n" | sudo tee -a /etc/sysctl.d/99-xmrmining.conf
 	echo -e "#Disable on all interfaces\nnet.ipv6.conf.all.disable_ipv6 = 1" | sudo tee -a /etc/sysctl.d/99-xmrmining.conf
 	sudo sysctl -p /etc/sysctl.d/99-xmrmining.conf
-	return $?
 fi
-}
 
 #Limits.conf
-function limitsconf ()
-{
 if grep -q "#Limits" /etc/security/limits.conf; then
-	echo -e "\nLimits ja configurado anteriormente.\n"
-	return 12
+	echo -e "\nLimits ja configurado anteriormente."
 else
 	echo -e "\n#Limits para mining\n* soft memlock 262144\n* hard memlock 262144" | sudo tee -a /etc/security/limits.conf
-	return $?
-fi
-}
-
-tput setaf 7 ; tput setab 2 ; tput bold ; printf '%35s%s%-20s\n' "XMR-Stak-CPU Setup" ; tput sgr0
-tput setaf 3 ; tput bold ; echo -e "Este script ira compilar o xmr-stak-cpu, fazer configuracoes no sysctl.conf e\n" ; tput sgr0
-tput setaf 3 ; tput bold ; echo -e "/etc/security/limits.conf e instalar alguns pacotes uteis.\n" ; tput sgr0
-tput setaf 3 ; tput bold ; echo -e "A instalacao iniciara em 3 segundos\n\n" ; tput sgr0 ; sleep 3
-
-#ConfiguraÁ„o inicial
-inicialconf
-
-#Compilacao do Stak
-stakmake
-retorno=$?
-if [ "$retorno" = "0" ]; then
-	echo -e "\n\nXMR-Stak-CPU Compilado!"
-	sleep 1
-else
-	echo -e "\n\nErro ao compilar! Saindo..."
-	exit 1
-fi
-echo -e "\n\nAgora as configuracoes finais."
-#Sysctl
-confsysctl
-retorno=$?
-if [ "$retorno" = "0" ]; then
-	echo -e "\nSysctl configurado!"
-	sleep 1
-else
-	echo -e "\nErro ao configurar Sysctl.conf. Saindo..."
-	exit 1
 fi
 
-#Limits
-limitsconf
-retorno=$?
-if [ "$retorno" = "0" ]; then
-	echo -e "\nLimits.conf configurado!"
-	sleep 1
-else
-	echo -e "\nErro ao configurar Limits.conf. Saindo..."
-	exit 1
-fi
-
-echo -e "\nFinalizado!"
-echo -e "\nAgora voce precisa ajustar as configuracoes do config.txt do xmr-stak."
-echo -e "\nAntes, reinicie a VPS para que as configuracoes facam efeito!\n"
-echo -e "\nAte mais!\n"
+echo -e "\nFinalizado!\n"
+echo "Agora voce precisa ajustar as configuracoes do config.txt do xmr-stak."
+echo "Antes, reinicie a VPS para que as configura√ß√µes fa√ßam efeito!\n"
+echo -e "Ate mais!\n"
+exit 0
